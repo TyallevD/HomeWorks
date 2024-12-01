c6633ca41f99db87e2082f2eb21de4a86f75d492
Дмитрий <disturbeddokubo25@gmail.com>
2024-12-01 16:23:17 +0300

    HomeWork13 started

---
+++ src/HomeWork13.java
@@ -0,0 +1,89 @@
+public class HomeWork13 {
+    public static void main(String[] args) {
+        //1) lTrim
+        System.out.println("Задание 1. Вывод:");
+        System.out.println();
+
+        //2) RTrim
+        System.out.println("Задание 2. Вывод:");
+        System.out.println();
+
+        //3) Trim
+        System.out.println("Задание 3. Вывод:");
+        System.out.println();
+
+        //4) equals
+        System.out.println("Задание 4. Вывод:");
+        System.out.println();
+
+        //5) startsWith
+        System.out.println("Задание 5. Вывод:");
+        System.out.println();
+
+        //6) replace
+        System.out.println("Задание 6. Вывод:");
+        System.out.println();
+
+        //7) equalsIgnoreCase
+        System.out.println("Задание 7. Вывод:");
+        System.out.println();
+
+        //8) concat
+        System.out.println("Задание 8. Вывод:");
+        System.out.println();
+
+        //9) endsWith
+        System.out.println("Задание 9. Вывод:");
+        System.out.println();
+
+        //10) substring
+        System.out.println("Задание 10. Вывод:");
+        System.out.println();
+
+        //11) toCharArray
+        System.out.println("Задание 11. Вывод:");
+        System.out.println();
+
+        //12) indexOf( char a)
+        System.out.println("Задание 12. Вывод:");
+        System.out.println();
+
+        //13) indexOf( char a , int fromIndex)
+        System.out.println("Задание 13. Вывод:");
+        System.out.println();
+
+        //14) lastIndexOf( char a)
+        System.out.println("Задание 14. Вывод:");
+        System.out.println();
+
+        //15) lastIndexOf( char a , int fromIndex)
+        System.out.println("Задание 15. Вывод:");
+        System.out.println();
+
+        //16) lastIndexOf( char[] a)
+        System.out.println("Задание 16. Вывод:");
+        System.out.println();
+
+        //17) indexOf( char[] a)
+        System.out.println("Задание 17. Вывод:");
+        System.out.println();
+
+        //18) indexOf( char[] a , int fromIndex)
+        System.out.println("Задание 18. Вывод:");
+        System.out.println();
+
+        //19) lastIndexOf( char[] a , int fromIndex)
+        System.out.println("Задание 19. Вывод:");
+        System.out.println();
+
+        //20) contains
+        System.out.println("Задание 20. Вывод:");
+        System.out.println();
+
+        //21) split
+        System.out.println("Задание 21. Вывод:");
+        System.out.println();
+
+
+    }
+}
