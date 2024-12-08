7038ab4066d0c7bedae45558a28eb04032c2271a
Дмитрий <disturbeddokubo25@gmail.com>
2024-12-08 17:59:39 +0300

    HomeWork 14 and 15 added

---
+++ src/HomeWork14.java
@@ -0,0 +1,6 @@
+public class HomeWork14 {
+    public static void main(String[] args) {
+        // public static char[] replaceAll(char[] original,char[] oldStr,char[] newStr)
+        //реализовать этот метод используя ( либо while ) (либо   do while)
+    }
+}
