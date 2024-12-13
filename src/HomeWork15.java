1f09d89fceba00c2286fa26db5742eee1668730f
Дмитрий <disturbeddokubo25@gmail.com>
2024-12-13 15:18:30 +0300

    HomeWork15 completed

--- src/HomeWork15.java
+++ src/HomeWork15.java
@@ -1,44 +1,26 @@
+import java.util.Arrays;
+
 public class HomeWork15 {
-    public static int[] concat(int[] one, int[] two) {
-        //{-2,-1,0,4,7,10,12};
-        //{-10,1,1,1,2,3,6,7,10,100,1000};
-
-//        {-1, 0, 2};
-//        {-10, -2, 1, 4};
+    public static int[] myConcat(int[] one, int[] two) {
         int[] result = new int[one.length + two.length];
-        int min1 = 0;
-        int min2 = 0;
+        int min1 = 0; //начальный индекс в массиве 1
+        int min2 = 0; // начальный индекс в массиве 2
 
-        for (int i = 0; i < result.length; i++) {
-            if (one[min1] > two[min2]) { //&& min2 <= two.length -1
-                System.out.println("Значение в индексе min1 больше min2: " + one[min1] + " > " + two[min2]);
-                System.out.println("Ставим в " + i + " результат = " + two[min2]);
+        for (int i = 0; i < result.length; i++) { //цикл для заполнения результирующего массива
+            if (min1 >= one.length) { // если вышли за предел массива 1, то в результат записываем оставшиеся значения из массива 2
                 result[i] = two[min2];
-                System.out.println("Прибавляем min2 " + min2);
-                min2++;
-                System.out.println("min2 = " + min2);
-                System.out.println();
-
-            } else  {// if (one[min1] <= two[min2]) //&& min1 <= one.length-1
-                System.out.println("Значение в индексе min1 меньше min2: " + one[min1] + " < " + two[min2]);
-                System.out.println("Ставим в " + i + " результат = " + one[min1]);
+                min2++; //переходим на следующий индекс в массиве 2
+            } else if (min2 >= two.length) { //иначе если вышли за предел массива 2, то в результат записываем оставшиеся значения из массива 1
                 result[i] = one[min1];
-                System.out.println("Прибавляем min1 " + min1);
-                min1++;
-                System.out.println("min1 = " + min1);
-                System.out.println();
+                min1++; //переходим на следующий индекс в массиве 1
+            } else if (one[min1] > two[min2]) { //иначе, если не вышли за пределы массивов - если значение в индексе массива 1 > значения в индексе массива 2, то записываем значение из массива 2
+                result[i] = two[min2];
+                min2++; //переходим на следующий индекс в массиве 2
+            } else { // иначе, если не вышли за пределы массивов - если значение в индексе массива 1 меньше или равно значению в индексе массива 2, то записываем в результат значение из массива 1
+                result[i] = one[min1];
+                min1++; //переходим на следующий индекс в массиве 1
             }
-//            if (min1 == one.length) {
-//                result[i] = two[min2];
-//                min2++;
-//            }
-//            if (min2 == two.length) {
-//                result[i] = one[min1];
-//                min1++;
-//            }
         }
-        //TODO Заполнение происходит частично, далее упираюсь в размер первого массива. А что если первый массив больше второго? Или равны?
-
         return result;
     }
 
@@ -63,12 +45,15 @@ public class HomeWork15 {
         //Какие либо готовые методы
         //Сортировку
 
-//        int[] arr1 = {-2, -1, 0, 4, 7, 10, 12};
-//        int[] arr2 = {-10, 1, 1, 1, 2, 3, 6, 7, 10, 100, 1000};
-        int[] arr1 = {-1, -0, 2};
-        int[] arr2 = {-10, -2, 1, 4};
+        int[] arr1 = {-2, -1, 0, 4, 7, 10, 12};
+        int[] arr2 = {-10, 1, 1, 1, 2, 3, 6, 7, 10, 100, 1000};
 
-        int[] arr3 = concat(arr1, arr2);
+        System.out.println("Массив 1: " + Arrays.toString(arr1));
+        System.out.println("Массив 2: " + Arrays.toString(arr2));
+
+        int[] arr3 = myConcat(arr1, arr2);
+
+        System.out.print("Выводим соединенный массив: ");
         for (int i = 0; i < arr3.length; i++) {
             System.out.print(arr3[i] + " ");
         }
