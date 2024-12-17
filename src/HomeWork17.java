110e1071e84ad503f4db9a49948b2ab09a7280e6
Дмитрий <disturbeddokubo25@gmail.com>
2024-12-17 23:42:30 +0300

    HomeWork17 6/10

--- src/HomeWork17.java
+++ src/HomeWork17.java
@@ -5,42 +5,82 @@ public class HomeWork17 {
         //Поле radius (тип double).
         //Конструктор для установки радиуса.
         //Метод calculateArea(), который возвращает площадь круга.
+        System.out.println("Задание 1. Вывод:");
+        double radius = 10;
 
+        Circle circle = new Circle(radius);
+
+        System.out.println("Площадь круга с радиусом " + radius + " равна: " + circle.calculateArea());
+        System.out.println();
 
         //Задание 2
         //Создайте класс Rectangle, который имеет:
         //Поля length и width (оба типа double).
         //Конструктор для установки длины и ширины.
         //Метод calculatePerimeter(), который возвращает периметр прямоугольника.
+        System.out.println("Задание 2. Вывод:");
+        double length = 10;
+        double width = 10;
 
+        Rectangle rectangle = new Rectangle(length, width);
+
+        System.out.println("Периметр прямоугольника с длиной \"" + length + "\" и шириной \"" +
+                width + "\" равна " + rectangle.calculatePerimeter());
+        System.out.println();
 
         //Задание 3
         //Создайте класс Student, который имеет:
         //Поля name (тип String) и age (тип int).
         //Конструктор для установки имени и возраста.
         //Метод displayInfo(), который выводит имя и возраст студента в консоль.
+        System.out.println("Задание 3. Вывод:");
 
+        Student student = new Student("Дмитрий", 34);
+        student.displayInfo();
+
+        System.out.println();
 
         //Задание 4
         //Создайте класс Car, который имеет:
         //Поля brand (тип String) и speed (тип int).
         //Конструктор для установки бренда и начальной скорости.
         //Метод accelerate(int increment), который увеличивает скорость на указанное значение.
+        System.out.println("Задание 4. Вывод:");
 
+        Car car1 = new Car("Мерседес", 0);
+
+        System.out.println("Автомобиль " + car1.brand + ". Скорость " + car1.speed + " км/ч");
+        System.out.println("Разгоняем " + car1.brand + " до " + car1.accelerate(60) + " км/ч");
+        System.out.println("Теперь " + car1.brand + " едет со скоростью " + car1.speed + " км/ч");
+        System.out.println();
 
         //Задание 5
         //Создайте класс Book, который имеет:
         //Поля title (тип String) и author (тип String).
         //Конструктор для установки названия книги и автора.
         //Метод getBookInfo(), который возвращает строку с информацией о книге.
+        System.out.println("Задание 5. Вывод:");
 
+        Book book1 = new Book("Java. Полное руководство для начинающих", "Герберт Шилдт");
+        System.out.println(book1.getBookInfo());
+        Book book2 = new Book("Властелин колец", "Дж. Р. Р. Толкиен");
+        System.out.println(book2.getBookInfo());
 
-        //Задание 6
+        System.out.println();
+
+        //Задание 6 //TODO дооформить
         //Создайте класс Point, который имеет:
         //Поля x и y (оба типа int).
         //Конструктор для установки координат точки.
         //Метод move(int dx, int dy), который изменяет координаты точки на заданное смещение.
-
+        System.out.println("Задание 6. Вывод:");
+        Point point = new Point(0,0);
+        System.out.println(point.x +" "+ point.y);
+        point.move(1,2);
+        System.out.println(point.x +" "+ point.y);
+        point.move(-1,2);
+        System.out.println(point.x +" "+ point.y);
+        System.out.println();
 
         //Задание 7
         //Создайте класс Person, который имеет:
@@ -69,5 +109,92 @@ public class HomeWork17 {
         //Поля hours и minutes (оба типа int).
         //Конструктор для установки времени.
         //Метод addMinutes(int minutes), который добавляет заданное количество минут к времени и корректно обновляет часы.
+    }
+}
+
+//Класс для задания 1
+class Circle {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+}
+
+//Класс для задания 2
+class Rectangle {
+    double length;
+    double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+//Класс для задания 3
+class Student {
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println(name + " " + age);
+    }
+}
+
+//Класс для задания 4
+class Car {
+    String brand;
+    int speed;
+
+    public Car(String brand, int speed) {
+        this.brand = brand;
+        this.speed = speed;
+    }
+
+    public int accelerate(int increment) {
+        return speed += increment;
+    }
+}
+
+//Класс для задания 5
+class Book {
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getBookInfo() {
+        return "Книгу под названием \"" + title + "\" написал " + author;
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void move(int dx, int dy){
+        x += dx;
+        y += dy;
     }
 }
