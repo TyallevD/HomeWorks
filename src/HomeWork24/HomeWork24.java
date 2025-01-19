d9bbdb27af7f2a700bd16d9091643899046eec07
Дмитрий <disturbeddokubo25@gmail.com>
2025-01-19 16:24:23 +0300

    HomeWork24 added

---
+++ src/HomeWork24/HomeWork24.java
@@ -0,0 +1,14 @@
+package HomeWork24;
+
+public class HomeWork24 {
+    public static void main(String[] args) {
+        //Realizovat Dynamic Array
+        // +  1 add(T value)
+        // -  2 add(int index , T value)
+        // -  3 size()
+        // -  4 get(int index)
+        // -  5 clear()
+        // +  6 remove(T value)
+        // -  7 remove(int index)
+    }
+}
