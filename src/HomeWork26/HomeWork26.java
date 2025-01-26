4977f6ede376dab08676068bfb5b1a56abd352f7
Дмитрий <disturbeddokubo25@gmail.com>
2025-01-26 21:02:26 +0300

    HomeWork26 added

---
+++ src/HomeWork26/HomeWork26.java
@@ -0,0 +1,15 @@
+package HomeWork26;
+
+public class HomeWork26 {
+    public static void main(String[] args) {
+//   1   public void clear()
+//   2   public boolean isHasValue(int value)
+//   3   public int indexOf(int value)
+//   4   public void removeByIndex(int index)
+//   5   public void removeByValue(int value)
+//   6   public void addAll(Integer[] arr)
+//   7   public void removeAllDuplicate()
+//   8   public void decreaseArr()
+
+    }
+}
