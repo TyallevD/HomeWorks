bd4a1e808ce66876255452782ed0e98886704dad
Дмитрий <disturbeddokubo25@gmail.com>
2025-02-08 15:48:30 +0300

    HomeWork29 added

---
+++ src/HomeWork29/HomeWork29.java
@@ -0,0 +1,77 @@
+package HomeWork29;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+class MySet<E> {
+    private ArrayList<E> elements;  // Динамический массив для хранения элементов
+
+    // Конструктор
+    public MySet() {
+        elements = new ArrayList<>();
+    }
+
+    // 1. Добавляет элемент в множество, если его еще нет
+    public boolean add(E e) {
+        // TODO: Реализация
+        return false;
+    }
+
+    // 2. Удаляет элемент из множества
+    public boolean remove(Object o) {
+        // TODO: Реализация
+        return false;
+    }
+
+    // 3. Проверяет, содержит ли множество указанный элемент
+    public boolean contains(Object o) {
+        // TODO: Реализация
+        return false;
+    }
+
+    // 4. Возвращает количество элементов в множестве
+    public int size() {
+        // TODO: Реализация
+        return 0;
+    }
+
+    // 5. Проверяет, пусто ли множество
+    public boolean isEmpty() {
+        // TODO: Реализация
+        return false;
+    }
+
+    // 6. Удаляет все элементы из множества
+    public void clear() {
+        // TODO: Реализация
+    }
+
+    // 7. Добавляет все элементы из указанной коллекции в множество
+    public boolean addAll(Collection<? extends E> c) {
+        // TODO: Реализация
+        return false;
+    }
+
+    // 8. Проверяет, содержит ли множество все элементы из указанной коллекции
+    public boolean containsAll(Collection<?> c) {
+        // TODO: Реализация
+        return false;
+    }
+
+    // 9. Удаляет из множества все элементы, которые есть в указанной коллекции
+    public boolean removeAll(Collection<?> c) {
+        // TODO: Реализация
+        return false;
+    }
+
+    // 10. Сохраняет только те элементы, которые есть в указанной коллекции
+    public boolean retainAll(Collection<?> c) {
+        // TODO: Реализация
+        return false;
+    }
+}
+public class HomeWork29 {
+    public static void main(String[] args) {
+
+    }
+}
