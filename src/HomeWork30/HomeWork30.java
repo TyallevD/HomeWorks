92cb8f23f52b51c5cee23d068d40051cc24e7419
Дмитрий <disturbeddokubo25@gmail.com>
2025-02-11 22:59:54 +0300

    HomeWork30 3/6 completed

--- src/HomeWork30/HomeWork30.java
+++ src/HomeWork30/HomeWork30.java
@@ -6,6 +6,18 @@ import java.util.Scanner;
 
 public class HomeWork30 {
     public static void main(String[] args) {
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //1) cls -> очищает весь словарь
+        //2) size -> печатает количество слов в словаре
+        //3) letter -> печатает количество символов в словаре
+        //4) num -> печатает сколько чисел в словаре
+        //5) delete -> удаляет по ключу или по значению
+        //6) update -> изменит слово на что-то
+        //   6.1 = если пара с таким же key и value есть -> ошибка, что данная пара уже существует
+        //   6.2 = если первое введенное слово совпадает с ключом -> обновит только значение
+        //   6.3 = если первое введенное слово совпадает со значением -> обновит ключ
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         Map<String, String> dictionary = new LinkedHashMap<>();
         dictionary.put("dog", "собака");
         dictionary.put("mother", "мама");
@@ -21,25 +33,13 @@ public class HomeWork30 {
             String word = scanner.nextLine();
             word = word.toLowerCase();
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            //1) cls -> очищает весь словарь
-            //2) size -> печатает количество слов в словаре
-            //3) letter -> печатает количество символов в словаре
-            //4) num -> печатает сколько чисел в словаре
-            //5) delete -> удаляет по ключу или по значению
-            //6) update -> изменит слово на что-то
-            //   6.1 = если пара с таким же key и value есть -> ошибка, что данная пара уже существует
-            //   6.2 = если первое введенное слово совпадает с ключом -> обновит только значение
-            //   6.3 = если первое введенное слово совпадает со значением -> обновит ключ
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
             if (word.equals("command")) {
                 System.out.print("Введите команду (print, cls, size, letter, num, delete, update, exit): ");
                 word = scanner.nextLine();
                 word = word.toLowerCase();
 
                 if (word.equals("print")) {
+//                    printDictionary(dictionary);
                     System.out.println("\n-----------------------------------------\nПечать словаря\n");
                     for (String key : dictionary.keySet()) {
                         System.out.println(key + " (en) => " + dictionary.get(key) + " (ru)");
@@ -48,6 +48,55 @@ public class HomeWork30 {
                 if (word.equals("exit")) {
                     System.out.println("Вы вышли из программы");
                     break;
+                }
+
+                //1) cls -> очищает весь словарь
+                if (word.equals("cls")) {
+                    dictionary.clear();
+                }
+
+                //2) size -> печатает количество слов в словаре
+                if (word.equals("size")) {
+//                    sizeCount(dictionary);
+                    int count = 0;
+                    for (String key : dictionary.keySet()) {
+                        count++;
+                    }
+                    System.out.println(count);
+                }
+
+                //3) letter -> печатает количество символов в словаре
+                if (word.equals("letter")) {
+                }
+
+                //4) num -> печатает сколько чисел в словаре
+                if (word.equals("num")) {
+
+                }
+
+                //5) delete -> удаляет по ключу или по значению
+                if (word.equals("delete")) {
+                    System.out.print("Какое слово хотите удалить?: ");
+                    String delWord = scanner.nextLine().toLowerCase();
+
+                    if (dictionary.containsKey(delWord)) {
+                        dictionary.remove(delWord);
+                    } else if (dictionary.containsValue(delWord)) {
+                        for (String key : dictionary.keySet()) {
+                            if (dictionary.get(key).equals(delWord)) {
+                                System.out.println("Слово " + dictionary.get(key) + " было удалено");
+                                dictionary.remove(key);
+                                break;
+                            }
+                        }
+                    } else {
+                        System.out.println("Такого слова нет в словаре.");
+                    }
+                }
+
+                //6) update -> изменит слово на что-то
+                if (word.equals("update")) {
+
                 }
 
             } else if (dictionary.containsKey(word)) {
@@ -84,7 +133,6 @@ public class HomeWork30 {
                         } else {
                             System.out.println("Слово не было добавлено, вы выполнили неверный выбор");
                         }
-
                         System.out.println("Слово успешно добавлено");
                     }
                 } else if (select == 2) {
@@ -92,9 +140,22 @@ public class HomeWork30 {
                 } else {
                     System.out.println("Вы сделали неверный выбор, выберете 1 или 2");
                 }
-
             }
-
         } while (true);
+    }
+
+    private static void sizeCount(Map<String, String> dictionary) {
+        int count = 0;
+        for (String key : dictionary.keySet()) {
+            count++;
+        }
+        System.out.println(count);
+    }
+
+    private static void printDictionary(Map<String, String> dictionary) {
+        System.out.println("\n-----------------------------------------\nПечать словаря\n");
+        for (String key : dictionary.keySet()) {
+            System.out.println(key + " (en) => " + dictionary.get(key) + " (ru)");
+        }
     }
 }
