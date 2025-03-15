e3589379df9584f99ac8f3cbe4984671e9beae9e
Дмитрий <disturbeddokubo25@gmail.com>
2025-03-15 17:22:02 +0300

    HomeWork34 main function is created, need improvements

--- src/HomeWork34/HomeWork34.java
+++ src/HomeWork34/HomeWork34.java
@@ -12,29 +12,38 @@ public class HomeWork34 {
         List<String> list = new ArrayList<>();
         System.out.print("Введите количество всех фильмов в библиотеке: ");
 //        int totalMovie = scanner.nextInt();
-        int totalMovie = 9; //тестовая строка
+        int totalMovie = 99; //тестовая строка
+        System.out.println(totalMovie);
 
         for (int i = 1; i <= totalMovie; i++) {
             list.add("Movie № " + i);
-            System.out.println("Movie № " + i);
+//            System.out.println("Movie № " + i);
         }
 //        System.out.println();
+
         //Вводим количество фильмов на одной странице
         System.out.print("Введите количество фильмов на одной странице: ");
         //int pageSize = scanner.nextInt();
-        int pageSize = 2; //тестовый вариант, 2 фильма на странице
-        System.out.println();
+        int pageSize = 10; //тестовый вариант, 2 фильма на странице
+        System.out.println(pageSize);
+//        System.out.println();
 
         //Вводим страницу, на которой хотим получить список фильмов
         System.out.print("Введите номер страницы: ");
         //int pageNum = scanner.nextInt();
-        int pageNum = 3; //тестовый вариант, 3-я страница, должно выводиться фильм 5 и 6
-        System.out.println();
+        int pageNum = 10; //тестовый вариант, 3-я страница, должно выводиться фильм 5 и 6
+        System.out.println(pageNum);
+//        System.out.println();
+
         //Реализация
-        //todo
-        List<String> result = list;
+        //нужно вернуть элементы, находящиеся на странице pageNum, в количестве pageSize
+
+        List<String> result = list.stream().distinct().skip(pageSize * (pageNum - 1)).limit(pageSize)
+                .toList();
+        //todo надо обработать, если страница 0 или меньше - ошибка, если страница ничего не содержит - вывести "ничего не найдено"
 
         //Результат
+        System.out.println("Найденные фильмы:");
         for (String movie : result) {
             System.out.println(movie);
         }
