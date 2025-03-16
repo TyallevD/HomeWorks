b6847e64151ce51882dbe2e52376560c72636bb1
Дмитрий <disturbeddokubo25@gmail.com>
2025-03-16 16:35:16 +0300

    HomeWork34 completed, HomeWork35 added 8/10, need improvements

--- src/HomeWork34/HomeWork34.java
+++ src/HomeWork34/HomeWork34.java
@@ -11,41 +11,50 @@ public class HomeWork34 {
         //генерируется список фильмов
         List<String> list = new ArrayList<>();
         System.out.print("Введите количество всех фильмов в библиотеке: ");
-//        int totalMovie = scanner.nextInt();
-        int totalMovie = 99; //тестовая строка
-        System.out.println(totalMovie);
+        int totalMovie = scanner.nextInt();
+//        int totalMovie = 9; // строка для тестирования
+//        System.out.println(totalMovie);
+//        System.out.println();
 
         for (int i = 1; i <= totalMovie; i++) {
             list.add("Movie № " + i);
-//            System.out.println("Movie № " + i);
         }
-//        System.out.println();
 
         //Вводим количество фильмов на одной странице
         System.out.print("Введите количество фильмов на одной странице: ");
-        //int pageSize = scanner.nextInt();
-        int pageSize = 10; //тестовый вариант, 2 фильма на странице
-        System.out.println(pageSize);
+        int pageSize = scanner.nextInt();
+//        int pageSize = 2; // строка для тестирования
+//        System.out.println(pageSize);
 //        System.out.println();
 
         //Вводим страницу, на которой хотим получить список фильмов
         System.out.print("Введите номер страницы: ");
-        //int pageNum = scanner.nextInt();
-        int pageNum = 10; //тестовый вариант, 3-я страница, должно выводиться фильм 5 и 6
-        System.out.println(pageNum);
+        int pageNum = scanner.nextInt();
+//        int pageNum = 3; // строка для тестирования
+//        System.out.println(pageNum);
 //        System.out.println();
 
         //Реализация
-        //нужно вернуть элементы, находящиеся на странице pageNum, в количестве pageSize
-
-        List<String> result = list.stream().distinct().skip(pageSize * (pageNum - 1)).limit(pageSize)
+        //проверки на корректность ввода
+        if (pageNum <= 0) {
+            System.out.println("Некорректная страница");
+            return;
+        }
+        if (pageSize <= 0) {
+            System.out.println("Некорректное число фильмов на странице");
+            return;
+        }
+        List<String> result = list.stream().skip(pageSize * (pageNum - 1)).limit(pageSize)
                 .toList();
-        //todo надо обработать, если страница 0 или меньше - ошибка, если страница ничего не содержит - вывести "ничего не найдено"
 
         //Результат
-        System.out.println("Найденные фильмы:");
-        for (String movie : result) {
-            System.out.println(movie);
+        if (result.isEmpty()) {
+            System.out.println("Ничего не найдено");
+        } else {
+            System.out.println("Найденные фильмы:");
+            for (String movie : result) {
+                System.out.println(movie);
+            }
         }
     }
 }
