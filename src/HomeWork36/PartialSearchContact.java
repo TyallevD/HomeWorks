6dc2b38680f39cff42e3b0261ac688b85076d638
Дмитрий <disturbeddokubo25@gmail.com>
2025-04-07 12:46:41 +0300

    HomeWor36 partial search added, logger remaining

--- src/HomeWork36/PartialSearchContact.java
+++ src/HomeWork36/PartialSearchContact.java
@@ -1,23 +1,117 @@
 package HomeWork36;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class PartialSearchContact {
     public static void partialSearchByName(List<Contact> userContactList, String name) {
+int count = 0;
         if (name.contains("_")) {
-            System.out.println("поиск по количеству введенных символов и приставке/окончанию");
+            String regex = convertToPartialRegex(name);
+            Pattern pattern = Pattern.compile(regex);
+            System.out.println("------------------------------------");
+            System.out.println(" Найденные контакты по шаблону имени \"" + name + "\"");
+            System.out.println("------------------------------------");
+            for (Contact contact : userContactList) {
+                if (pattern.matcher(contact.getName()).matches()) {
+                    count++;
+                    System.out.println(contact);
+                }
+            }
+            System.out.println("Всего найдено: " + count);
         } else if (name.contains("%")) {
-            System.out.println("Поиск по любому количеству символов");
+            String regex = convertToFullRegex(name);
+            Pattern pattern = Pattern.compile(regex);
+            System.out.println("------------------------------------");
+            System.out.println(" Найденные контакты по шаблону имени \"" + name + "\"");
+            System.out.println("------------------------------------");
+            for (Contact contact : userContactList) {
+                if (pattern.matcher(contact.getName()).matches()) {
+                    count++;
+                    System.out.println(contact);
+                }
+            }
+            System.out.println("Всего найдено: " + count);
+        } else {
+            System.out.println("По заданному шаблону ничего не найдено");
         }
     }
 
     public static void partialSearchBySurname(List<Contact> userContactList, String surname) {
-        //todo сделать поиск по символам "_" или "%"
+        int count = 0;
+        if (surname.contains("_")) {
+            String regex = convertToPartialRegex(surname);
+            Pattern pattern = Pattern.compile(regex);
+            System.out.println("------------------------------------");
+            System.out.println(" Найденные контакты по шаблону фамилии \"" + surname + "\"");
+            System.out.println("------------------------------------");
+            for (Contact contact : userContactList) {
+                if (pattern.matcher(contact.getSurname()).matches()) {
+                    count++;
+                    System.out.println(contact);
+                }
+            }
+            System.out.println("Всего найдено: " + count);
+        } else if (surname.contains("%")) {
+            String regex = convertToFullRegex(surname);
+            Pattern pattern = Pattern.compile(regex);
+            System.out.println("------------------------------------");
+            System.out.println(" Найденные контакты по шаблону фамилии \"" + surname + "\"");
+            System.out.println("------------------------------------");
+            for (Contact contact : userContactList) {
+                if (pattern.matcher(contact.getSurname()).matches()) {
+                    count++;
+                    System.out.println(contact);
+                }
+            }
+            System.out.println("Всего найдено: " + count);
+        } else {
+            System.out.println("По заданному шаблону ничего не найдено");
+        }
     }
 
     public static void partialSearchByPhone(List<Contact> userContactList, String phone) {
-        //todo сделать поиск по символам "_" или "%"
+        int count = 0;
+        if (phone.contains("_")) {
+            String regex = convertToPartialRegex(phone);
+            Pattern pattern = Pattern.compile(regex);
+            System.out.println("------------------------------------");
+            System.out.println(" Найденные контакты по шаблону телефона \"" + phone + "\"");
+            System.out.println("------------------------------------");
+            for (Contact contact : userContactList) {
+                if (pattern.matcher(contact.getPhone()).matches()) {
+                    count++;
+                    System.out.println(contact);
+                }
+            }
+            System.out.println("Всего найдено: " + count);
+        } else if (phone.contains("%")) {
+            String regex = convertToFullRegex(phone);
+            Pattern pattern = Pattern.compile(regex);
+            System.out.println("------------------------------------");
+            System.out.println(" Найденные контакты по шаблону телефона \"" + phone + "\"");
+            System.out.println("------------------------------------");
+            for (Contact contact : userContactList) {
+                if (pattern.matcher(contact.getPhone()).matches()) {
+                    count++;
+                    System.out.println(contact);
+                }
+            }
+            System.out.println("Всего найдено: " + count);
+        } else {
+            System.out.println("По заданному шаблону ничего не найдено");
+        }
+    }
 
+    private static String convertToPartialRegex(String stringPattern) {
+        String escapedString = stringPattern.replace("+", "\\+");
+        String regex = escapedString.replace("_", ".");
+        return "^" + regex + "$";
+    }
+
+    private static String convertToFullRegex(String stringPattern) {
+        String escapedString = stringPattern.replace("+", "\\+");
+        return escapedString.replace("%", ".*");
     }
 
     public static void partialSearchByAge(List<Contact> userContactList, int age, String direction) {
