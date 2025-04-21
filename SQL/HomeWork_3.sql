353366d7744693357214a78c4caba93638875d7b
Дмитрий <disturbeddokubo25@gmail.com>
2025-04-21 14:49:47 +0300

    SQL HomeWork_3 completed, HomeWork_4 added

--- SQL/HomeWork_3.sql
+++ SQL/HomeWork_3.sql
@@ -83,20 +83,88 @@
 --group by izd
 --having AVG(Price) < 300;
 
--- 18) ??? Найти книгу с минимальным значением Price * Pages.  
---select MIN(Price*Pages) as'Минимальное значение Price* Pages'
---from books
+-- 18) Найти книгу с минимальным значением Price * Pages.
+--изменил количество страниц на 1 во всех книгах с количеством =0, т.к. результат получается неочевидным
+--update books set pages = 1, price = 1 
+--where pages = 0 and pages is null and price is null;
 
-
--- 19) ??? Найти название самой старой книги по дате (Date). 
---select top 1 name
+--выполнение задания
+--select top 1 name, MIN(Price*Pages) as'Минимальное значение Price * Pages'
 --from books
 --group by name
---having Date = MAX(Date);
+--order by MIN (Price * Pages);
+
+
+-- 19) Найти название самой старой книги по дате (Date). 
+--select top 1  name, min(DATE) as 'Минимальная дата'
+--from books
+--group by name
+--having min(date) is not null
+--order by min(Date)
 
 -- 20) Найти издателя с наибольшим суммарным тиражом (Pressrun).
+--select top 1  izd, sum(Pressrun) as 'Суммарный тираж'
+--from books
+--group by izd
+--order by sum(Pressrun) desc;
+
 -- 21) Добавить новую книгу с названием 'Learning SQL', ценой 450, издательством 'EduBooks' и 320 страницами. 
+--insert into books (Name, Price, Izd, Pages)
+--values ('Learning SQL', 450, 'EduBooks', 320);
+
+-- Выводим результат:
+--select * 
+--from books 
+--where name = 'Learning SQL'; 
+
 -- 22) Добавить 3 книги, в которых цена = 999, но у всех разные издатели. 
--- 23) Удалить все книги, у которых название начинается с 'Test'. 
--- 24) Увеличить цену на 15% для всех книг, у которых Price < 300. 
+--insert into books (Price, Izd)
+--values (999, 'Первый издатель'),
+--(999, 'Второй издатель'),
+--(999, 'Третий издатель');
+
+--select * 
+--from books
+--where izd like '% издатель'; --для вывода результата
+
+-- 23) Удалить все книги, у которых название начинается с 'Test'.
+--добавляем, так как таких книг нет:
+--insert into books (name)
+--values	('Test1'),
+--		('Test2'),
+--		('Test 3');
+
+--проверяем
+--select *
+--from books
+--where name like 'Test%';
+
+--удаляем по заданию
+--delete from books 
+--where name like 'Test%';
+
+
+-- 24) Увеличить цену на 15% для всех книг, у которых Price < 300.
+--update books 
+--set price = price * 0.15 
+--where price <300;
+
 -- 25) Переименовать все книги с названием 'Old Title' на 'Modern SQL Guide'.
+--добавляем, т.к. такой книги в таблице нет:
+--insert into books (name)
+--values ('Old Title');
+
+--проверяем, что добавилась:
+--select * 
+--from books 
+--where name = 'Old Title';
+
+--изменяем название по заданию
+--update books
+--set name = 'Modern SQL Guide'
+--where name = 'Old Title';
+
+--проверяем, что книги с новым названием присутствуют в таблице:
+--select * 
+--from books 
+--where name = 'Modern SQL Guide';
