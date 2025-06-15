2f7ceb37d34ddfcee4f6cf64177a0779ac40ed89
Дмитрий <disturbeddokubo25@gmail.com>
2025-06-15 21:00:09 +0300

    JavaScript HomeWork_2 completed

--- JavaScript/HomeWork_2/assets/script/script.js
+++ JavaScript/HomeWork_2/assets/script/script.js
@@ -1,28 +1,58 @@
 let box = {
     element: document.querySelector('.box'),
-    top: 10,
-    left: 10,
-    isUp: false,
-    isLeft: false
-}
-setInterval(() => {
-    if (box.top < window.innerHeight - 165 && !box.isUp) {
-        console.log("Двигай вниз")
-        box.element.style.top = box.top + "px";
-        box.top += 10;
-    } else if (box.left < window.innerWidth - 165 && !box.isLeft) {
-        console.log("Двигай вправо")
-        box.isUp = true;
-        box.element.style.left = box.left + "px";
-        box.left += 10;
-    } else if (box.top > 0 && box.isUp) {
-        console.log("Двигай вверх")
-        box.isLeft = true;
-        box.element.style.top = box.top + "px";
-        box.top -= 10;
-    } else if (box.left > 0 && box.isLeft) {
-        console.log("Двигай влево")
-        box.element.style.left = box.left + "px";
-        box.left -= 10;
+    x: 0,
+    y: 0,
+    direction: 'down'
+};
+
+let iteration = 1;
+let lastIteration = 3;
+let moveAction;
+
+box.element.textContent = iteration;
+
+
+moveAction = setInterval(() => {
+    switch (box.direction) {
+        case 'down':
+            if (box.y < window.innerHeight - 165) {
+                box.y += 10;
+            } else {
+                box.direction = 'right';
+            }
+            break;
+        case 'right':
+            if (box.x < window.innerWidth - 165) {
+                box.x += 10;
+            } else {
+                box.direction = 'up';
+            }
+            break;
+        case 'up':
+            if (box.y > 0) {
+                box.y -= 10;
+            } else {
+                box.direction = 'left';
+            }
+            break;
+        case 'left':
+            if (box.x > 0) {
+                if (iteration == lastIteration) {
+                    box.element.textContent = 'Finish'
+                }
+                box.x -= 10;
+            } else {
+                if (iteration < lastIteration) {
+                    iteration++;
+                    box.element.textContent = iteration;
+                    box.direction = 'down';
+                } else {
+                    clearInterval(moveAction);
+                    box.element.remove();
+                }
+            }
     }
-}, 1 * 10);
+
+    box.element.style.top = box.y + 'px';
+    box.element.style.left = box.x + 'px';
+}, 1 * 10)
