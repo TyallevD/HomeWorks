2df42d8f1255cee83c87d937b7f2c6d8aa822ce6
Дмитрий <disturbeddokubo25@gmail.com>
2025-07-09 14:38:58 +0300

    HomeWork_6 updated to final version, HomeWork_7 added and completed

---
+++ JavaScript/HomeWork_7_exam/assets/script/omdbApi.js
@@ -0,0 +1,15 @@
+//http://www.omdbapi.com/?apikey=be8fee90&i=tt3896198
+let apikey = 'be8fee90';
+let baseURL = 'http://www.omdbapi.com';
+
+export async function searchByTitle(titleName, titleType,page) {
+    let response = await fetch(`${baseURL}/?apikey=${apikey}&s=${titleName}&type=${titleType}&page=${page}`);
+    let data = await response.json();
+    return data;
+}
+
+export async function loadDetails(id){
+    let responseById = await fetch(`${baseURL}/?apikey=${apikey}&i=${id}`);
+    let idData = await responseById.json();
+    return idData;
+}
