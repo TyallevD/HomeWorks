a4b0a676a37ef3658e01d4e6d9f48b9c0df04a9c
Дмитрий <disturbeddokubo25@gmail.com>
2025-07-27 19:44:16 +0300

    JavaBackend HomeWork2 updated

--- src/JavaBackend/HomeWork2/HomeWork2.java
+++ src/JavaBackend/HomeWork2/HomeWork2.java
@@ -3,5 +3,10 @@ package JavaBackend.HomeWork2;
 public class HomeWork2 {
     public static void main(String[] args) {
         //повторить порождающие паттерны
+        //Фабричный метод
+        //Абстрактная фабрика
+        //Строитель
+        //Прототип
+        //Одиночка
     }
 }
